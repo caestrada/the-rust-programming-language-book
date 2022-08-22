1b8e8a566503658208d0e7c3ea335b6a5ac674ea
Carlos Estrada <carlos.estrada@coretechnologies.io>
2022-08-21 22:39:06 -0400

    Refactor code

--- ch-12_an-io-project/minigrep/src/main.rs
+++ ch-12_an-io-project/minigrep/src/main.rs
@@ -8,12 +8,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let _contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
