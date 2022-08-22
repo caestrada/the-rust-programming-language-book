04fcc66cf08cd0f506fb2edbf4652c2292ee7bbe
Carlos Estrada <carlos.estrada@coretechnologies.io>
2022-08-22 07:41:01 -0400

    Extract logic from main

--- ch-12_an-io-project/minigrep/src/main.rs
+++ ch-12_an-io-project/minigrep/src/main.rs
@@ -17,8 +17,14 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let _contents = fs::read_to_string(config.file_path)
+    run(config);
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
 
 struct Config {
